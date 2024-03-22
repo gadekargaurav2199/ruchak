2ab35da0799a3083327efd6f6a1ad7351a98cfbb
gadekargaurav2199 <gadekargaurav2199@gmail.com>
2024-03-22 08:18:21 +0530

    22-03-14

--- ruchak/settings.py
+++ ruchak/settings.py
@@ -17,7 +17,9 @@ import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 
 
@@ -86,7 +88,7 @@ WSGI_APPLICATION = 'ruchak.wsgi.application'
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
 
